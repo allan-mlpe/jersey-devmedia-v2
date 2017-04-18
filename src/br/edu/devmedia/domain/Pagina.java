7e641a4690e0a0ab8af09039c2617e64e5054f19
Allan Monteiro <allan_mlpe@hotmail.com>
2017-04-18 20:30:26 -0300

    Criação da classe Pagina para encapsular o retorno dos serviços

---
+++ src/br/edu/devmedia/domain/Pagina.java
@@ -0,0 +1,35 @@
+package br.edu.devmedia.domain;
+
+import java.util.List;
+
+/**
+ * Esta classe serve para encapsular o retorno dos serviços a fim
+ * de evitar o uso da classe GenericEntity necessária para o retorno
+ * de um ArrayList ou Vector, por exemplo.
+ * 
+ * Um exemplo prático do uso de uma GenericEntity pode ser encontrado
+ * no método listarNotas() da classe NotaService da versão 1 do curso.
+ * 
+ * @author Allan
+ *
+ */
+public class Pagina {
+	
+	private List<Lembrete> dados;
+
+	public Pagina() {
+		
+	}
+	
+	public Pagina(List<Lembrete> dados) {
+		this.dados = dados;
+	}
+
+	public List<Lembrete> getDados() {
+		return dados;
+	}
+
+	public void setDados(List<Lembrete> dados) {
+		this.dados = dados;
+	}
+}
