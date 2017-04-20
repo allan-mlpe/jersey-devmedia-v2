0503f72f42f5911600d48688aa0af0d48b12e756
Allan Monteiro <allan_mlpe@hotmail.com>
2017-04-20 20:33:09 -0300

    Correção de nome na coluna de id do lembrete.

--- database.sql
+++ database.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS `db_lembrete`;
 USE `db_lembrete`;
 
 CREATE TABLE `lembrete` (
-  `ID_NOTA` int(11) NOT NULL,
+  `ID_LEMBRETE` int(11) NOT NULL,
   `TITULO` varchar(100) NOT NULL,
   `DESCRICAO` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`ID_NOTA`)
